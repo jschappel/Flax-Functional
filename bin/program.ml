f8724c71d620fa643e20757a42651fb6ef5a564d
jschappel <jmschappel12@gmail.com>
2021-08-30 18:33:15 -0400

    Repl now works, found error in lexer for lex string

--- bin/program.ml
+++ bin/program.ml
@@ -1,19 +1,16 @@
 open Stdio
 open Lib
 
-(* open Lexer *)
-
 let run_env line =
   lexProgram line 
 
-
-let loop =
-  begin
-    Out_channel.printf "> ";
-    Out_channel.flush stdout ;
+let () = 
+  let interpret_input = lexProgram in
+  while true do
+    Out_channel.printf "> "; Out_channel.flush stdout;
     match In_channel.input_line stdin with
-      | Some(line) -> Out_channel.print_endline line
+      | Some("exit") -> exit 0
+      | Some(line) -> 
+        Out_channel.print_endline @@ token_list_to_string @@ interpret_input line;
       | None -> Out_channel.print_endline "NULL"
-  end
-
-let () = loop
+  done
