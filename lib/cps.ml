87b51d2f28dbfdda521fec0d619925503234d05c
jschappel <jmschappel12@gmail.com>
2022-04-21 12:21:58 -0400

    Updated biolerplate for cps

--- lib/cps.ml
+++ lib/cps.ml
@@ -3,19 +3,30 @@ module CPS : sig
   open Enviroment.Enviroment
 
   type program = expression
+  type 'v cont = ('v -> 'v)
 
+  (* Returns the value of the program *)
   val value_of_program : program -> value
 
-  val value_of : expression -> env -> value
+  (* Returns the value of the given expression useing a cps transformation *)
+  val value_of_k : expression -> env -> 'a cont -> value
 
 end = struct
   open CoreProgram
   open Enviroment.Enviroment
 
   type program = expression
+  type 'v cont = ('v -> 'v)
 
-  let value_of_program prog = BoolVal(false)
+  let unimplimented _ = raise @@ Failure "unimplimented!"
 
-  let value_of exp env = BoolVal(false)
+  let end_cont v = 
+    print_endline "End continuation";
+    v
+    
+  let rec value_of_program prog = value_of_k prog EmptyEnv end_cont
+
+  and value_of_k exp env cont = unimplimented ()
+
 
 end
